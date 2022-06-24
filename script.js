7c77957000faa64f09174f43bd4018d6677f6494
Matthew Heering <matthewheering@Matthews-MacBook-Pro.local>
2022-06-23 21:55:02 -0500

    basic outline of html file, skeleton for js, implemented bootstrap.  Now I need to add buttons for adding books, editing books, storing books to mongo database, api call to get images for books, and set up authentication with js passport.

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-let myLibrary = [];
+let myLibrary = [
+      {
+            Title: "Your Mom",
+            Author: "Earnest Hemmingway",
+            Read: true,
+            User: "matthew.c.heering01@gmail.com"
+      }, 
+      {
+            Title: "The Hitchhiker's Guide to the Galaxy",
+            Author: "Douglas Adams",
+            Read: true,
+            User: "matthew.c.heering01@gmail.com"
+      }, 
+      {
+            Title: "The Bible",
+            Author: "God", 
+            Read: false, 
+            User: "mandy.heering@gmail.com"
+      }
+];
 
 const Book = () => {
-//the constructor
+            this.Title = Title;
+            this.Author = Author;
+            this.Read = Read;
+            this.User = User;
+ 
 };
 
 const addBookToLibrary = () => {
-// do stuff here
+
 };
 
 
