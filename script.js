91df84441aa1e358823b1763b60d03d88be3f632
Matthew Heering <matthewheering@Matthews-MacBook-Pro.local>
2022-06-24 21:14:12 -0500

    functions for reading through JSON and building DOM elements comeplete.  Now I need to add functionality for writing to database, and signing in. THen I can add edit functionality.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cardContainer = document.querySelector('.cardContainer');
+
 let myLibrary = [
       {
             Title: "Your Mom",
@@ -30,5 +32,102 @@ const Book = () => {
 const addBookToLibrary = () => {
 
 };
+
+const addAttributes = (book) => {
+
+
+      book = document.createElement('div')
+      book.setAttribute('class', 'card')
+      book.setAttribute('style', 'width: 18rem;')
+      
+      const image = document.createElement('img')
+      image.setAttribute('class', 'card-img-top')
+      image.setAttribute('src', '')
+      
+      const infoDiv = document.createElement('div')
+      infoDiv.setAttribute('class', 'card-body')
+
+      const cardTitle = document.createElement('h5')
+      cardTitle.setAttribute('class', 'card-title')
+      cardTitle.innerHTML = book.Title
+
+      const cardAuthor = document.createElement('p')
+      cardAuthor.setAttribute('class', 'card-text')
+      cardAuthor.innerHTML =  book.Author
+
+      const cardReadStatus = document.createElement('p')
+      cardReadStatus.setAttribute('class', 'card-text')
+      cardReadStatus.innerHTML = (book.Read) ? "Read" : "Not Read"
+
+      const editButton = document.createElement('a')
+      editButton.setAttribute('class', 'btn btn-prmary')
+      editButton.setAttribute('href', '#')
+      editButton.innerHTML = "Edit"
+
+
+      const markStatusButton = document.createElement('a')
+      markStatusButton.setAttribute('class', 'btn btn-prmary')
+      markStatusButton.setAttribute('href', '#')
+      markStatusButton.innerHTML = "Mark as Read"
+
+      cardContainer.appendChild(book)
+      book.appendChild(image)
+      book.appendChild(infoDiv)
+      infoDiv.appendChild(cardTitle)
+      infoDiv.appendChild(cardAuthor)
+      infoDiv.appendChild(cardReadStatus)
+      book.appendChild(editButton)
+      book.appendChild(markStatusButton)
+      
+}
+
+const populateLibraryFromDatabase = (books) => {
+      for (let i = 0; i < books.length; i++) {
+      let book = document.createElement('div')
+      book.setAttribute('class', 'card')
+      book.setAttribute('style', 'width: 18rem;')
+      
+      const image = document.createElement('img')
+      image.setAttribute('class', 'card-img-top')
+      image.setAttribute('src', '')
+      
+      const infoDiv = document.createElement('div')
+      infoDiv.setAttribute('class', 'card-body')
+
+      const cardTitle = document.createElement('h5')
+      cardTitle.setAttribute('class', 'card-title')
+      cardTitle.innerHTML = books[i].Title
+
+      const cardAuthor = document.createElement('p')
+      cardAuthor.setAttribute('class', 'card-text')
+      cardAuthor.innerHTML =  books[i].Author
+
+      const cardReadStatus = document.createElement('p')
+      cardReadStatus.setAttribute('class', 'card-text')
+      cardReadStatus.innerHTML = (books[i].Read) ? "Read" : "Not Read"
+
+      const editButton = document.createElement('a')
+      editButton.setAttribute('class', 'btn btn-prmary')
+      editButton.setAttribute('href', '#')
+      editButton.innerHTML = "Edit"
+
+
+      const markStatusButton = document.createElement('a')
+      markStatusButton.setAttribute('class', 'btn btn-prmary')
+      markStatusButton.setAttribute('href', '#')
+      markStatusButton.innerHTML = "Mark as Read"
+
+      cardContainer.appendChild(book)
+      book.appendChild(image)
+      book.appendChild(infoDiv)
+      infoDiv.appendChild(cardTitle)
+      infoDiv.appendChild(cardAuthor)
+      infoDiv.appendChild(cardReadStatus)
+      book.appendChild(editButton)
+      book.appendChild(markStatusButton)
+      }
+}
+
+populateLibraryFromDatabase(myLibrary)
 
 
